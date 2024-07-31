8c6a318bb4a1e06135649c0cce07ddae618f1b70
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-07-31 20:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 let codes = document.querySelectorAll(".code")
-
+// change, input
 codes.forEach((code,index)=>{
-codes.addEventListener("input", ()=>{
-	if (code.value.length == 1) {
-		if (index < codes.length-1) {
-			codes[index+1].focus()
-		}
-	}
-})
-
-code.addEventListener("keydown", (e)=>{
-	if (e.key == "Backspace") {
-		if (index>0 && code.value.length == 0) {
-			codes[index-1].focus()
-		}
-	}
-})
-
+    
+    code.addEventListener("input",()=>{
+        if(code.value.length == 1){
+             if(index < codes.length-1){
+                   codes[index+1].focus()
+             }
+        }
+    })
+    // keyup, keydown(press)
+    code.addEventListener("keydown",(e)=>{
+           console.log(e.key)
+        if(e.key == "Backspace"){
+              if(index > 0 && code.value.length == 0){
+                codes[index-1].focus()
+              }
+        }
+    })
 })
