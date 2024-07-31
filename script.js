d904228e640a2d3f604adf01649e4627d553f37f
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-07-31 21:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-let codes = document.querySelectorAll(".code");
+        let codes = document.querySelectorAll(".code");
 
-codes.forEach((code, index) => {
-    code.addEventListener("input", () => {
-        if (code.value.length == 1) {
-            if (index < codes.length - 1) {
-                codes[index + 1].focus();
-                codes[index + 1].classList.add("focused");
-            }
-        }
-    });
+        codes.forEach((code, index) => {
+            code.addEventListener("input", () => {
+                if (code.value.length == 1) {
+                    if (index < codes.length - 1) {
+                        code.removeAttribute("focused");
+                        codes[index + 1].focus();
+                        codes[index + 1].setAttribute("focused", "");
+                    }
+                }
+            });
 
-    code.addEventListener("keydown", (e) => {
-        if (e.key == "Backspace") {
-            if (index > 0 && code.value.length == 0) {
-                codes[index - 1].focus();
-                codes[index - 1].classList.add("focused");
-            }
-        }
-    });
+            code.addEventListener("keydown", (e) => {
+                if (e.key == "Backspace") {
+                    if (index > 0 && code.value.length == 0) {
+                        code.removeAttribute("focused");
+                        codes[index - 1].focus();
+                        codes[index - 1].setAttribute("focused", "");
+                    }
+                }
+            });
 
-    code.addEventListener("focus", () => {
-        code.classList.add("focused");
-    });
+            code.addEventListener("focus", () => {
+                code.setAttribute("focused", "");
+            });
 
-    code.addEventListener("blur", () => {
-        code.classList.remove("focused");
-    });
-});
+            code.addEventListener("blur", () => {
+                code.removeAttribute("focused");
+            });
+        });
+
+        window.addEventListener('load', () => {
+            codes[0].focus();
+            codes[0].setAttribute("focused", "");
+        });
