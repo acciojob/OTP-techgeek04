6876eb91b3affebae1fa38749933f81b54fd8fa1
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-07-31 21:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-let codes = document.querySelectorAll(".code")
-// change, input
-codes.forEach((code,index)=>{
-    
-    code.addEventListener("input",()=>{
-        if(code.value.length == 1){
-             if(index < codes.length-1){
-                   codes[index+1].focus()
-                   codes[index+1].classList.add("focused")
-             }
+let codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+    code.addEventListener("input", () => {
+        if (code.value.length == 1) {
+            if (index < codes.length - 1) {
+                codes[index + 1].focus();
+                codes[index + 1].classList.add("focused");
+            }
         }
-    })
-    // keyup, keydown(press)
-    code.addEventListener("keydown",(e)=>{
-        if(e.key == "Backspace"){
-              if(index > 0 && code.value.length == 0){
-                
-                codes[index-1].focus()
-                codes[index-1].classList.add("focused")
-              }
+    });
+
+    code.addEventListener("keydown", (e) => {
+        if (e.key == "Backspace") {
+            if (index > 0 && code.value.length == 0) {
+                codes[index - 1].focus();
+                codes[index - 1].classList.add("focused");
+            }
         }
-    })
-    code.addEventListener("blur", ()=>{
-          code.classList.remove("focused")
-    })
-})
+    });
+
+    code.addEventListener("focus", () => {
+        code.classList.add("focused");
+    });
+
+    code.addEventListener("blur", () => {
+        code.classList.remove("focused");
+    });
+});
